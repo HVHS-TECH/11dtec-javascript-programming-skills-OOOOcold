38ff958fd0a3abaa11d08883ccba66a90982cdb6
OOOOcold <23541jl@hvhs.school.nz>
2025-05-05 21:20:51 +0000

    May 5, 2025, 9:20 PM

---
+++ t03_maths.js
@@ -0,0 +1,46 @@
+/**************************** 
+ Name of task: Intro to variables
+ Person who wrote it: Jason
+ Date written: 5/1/2025
+ Purpose: Introduction to math and how it works
+ ****************************/
+
+ //Variables 
+ var Year = 2025;
+console.log (Year);
+
+var Born = 2010;
+
+var Age = 15;
+console.log (Age);
+
+var Num1 = 10;
+
+var Num2 = 2;
+
+var answer;
+
+  let pocketMoney =3;
+  console.log("pocketMoney")
+
+  
+  console.log("Hi Jason");
+  console.log("As of", Year, "You are", Age);
+  console.log("You were born in", Born)
+  answer = Age + Num1;
+  console.log("In 10 years you will be", answer, "year old")
+  console.log("You have", pocketMoney,"dollars");
+  answer = pocketMoney/Num2
+  console.log("Half of your money is",answer )
+ 
+ 
+ /****************************
+  Main code 
+ ****************************/
+ 
+ 
+ 
+ 
+ /****************************
+  Functions
+ ****************************/
