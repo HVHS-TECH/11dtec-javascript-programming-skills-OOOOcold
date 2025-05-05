74754db54a622e76aa8d6f08adae60901656e226
OOOOcold <23541jl@hvhs.school.nz>
2025-05-05 21:13:25 +0000

    May 5, 2025, 9:13 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,21 @@
+/***************************
+ Name of task: javascrpt
+ Name : Jason Liu
+ Date : 30/04/25
+ Purpose: learning java script
+ * ************************/
+console.log("running T1-intro.js")
+
+//Variables
+
+
+/**************************
+ * main code
+ **************************/
 
 
 
-console.log("Running t01_introduction.js");
+
+/***************************** 
+ * functions
+ * ***************************/
---
+++ t02_variables.js
@@ -0,0 +1,21 @@
+/***************************
+ Name of task: javascrpt
+ Name : Jason Liu
+ Date : 30/04/25
+ Purpose: learning java script
+ * ************************/
+ 
+
+ //Variables
+ var=username
+ 
+ /**************************
+  * main code
+  **************************/
+ 
+ 
+ 
+ 
+ /***************************** 
+  * functions
+  * ***************************/
