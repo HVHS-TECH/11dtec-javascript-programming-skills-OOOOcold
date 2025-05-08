7b91e9d1cb9f91da879e05c4817fb8030143709a
OOOOcold <23541jl@hvhs.school.nz>
2025-05-08 01:14:23 +0000

    May 8, 2025, 1:14 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,18 +1,25 @@
-// Set up the size of the page
+
+console.log("t04-debugging.js")
+
+// calculate the area
+var area = 8 * 10;
+
+// calculate the perimeter
+var perimeter = 10 + 8; 
+
+// Set up the size of the page.
 var length = 8;
 var width = 10;
 
-// Calculate the area and perimeter
-var area = length * width;
-var perimeter = 2 * (length + width);
-
 // Display the results
-console.log("The area of a piece of paper with a length of " + length + " and a width of " + width + " is " + area);
-console.log("The same piece of paper has a perimeter of " +perimeter);
+console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
+console.log("The same piece of paper has a perimeter of "+perimeter)
 
-// Check if the result is sensible
-if (perimeter > area) {
-    console.log("Yay, your perimeter is larger, which is correct for this example.");
-} else {
-    console.log("Hmm, your area is larger. It shouldn't be for this example.");
+// Check, does this result look sensible
+if ( area > perimeter)
+{console.log("Yay, your area is larger, this is right for this example")
+}else
+{ 
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }
+
