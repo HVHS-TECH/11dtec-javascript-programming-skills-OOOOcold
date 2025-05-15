c5d6ea641b09b133705187c93bca5c5ce7c65e4b
OOOOcold <23541jl@hvhs.school.nz>
2025-05-15 00:53:11 +0000

    May 15, 2025, 12:53 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,4 @@
+prompt("Welcome to my program " +userName+ "!\nHow old are you?", 15);
+prompt("Thats great " +userName+ "!\nHow much pocket money do you have?", "5 dollars");
+alert("So if I'm not mistaken")
+alert("As of " +Year+ " you are " +Age+ " years old\nYou were born in 2010\nIn 10 years you will be 25 years old\nYou have " +Pocketmoney+ " dollars\nHalf of your pocket money is 2.5 dollars");
