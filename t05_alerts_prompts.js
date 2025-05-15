2af198637f75652c0bb74df7edf1b2bd4c8bf7a1
OOOOcold <23541jl@hvhs.school.nz>
2025-05-15 01:06:57 +0000

    May 15, 2025, 1:06 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -1,13 +1,47 @@
-var Age=15;
-var Year= 2010;
-var PocketMoney= 5;
 
+ //Variables 
+ var Year = 2025;
+console.log (Year);
 
+var Born = 2010;
 
+var Age = 15;
+console.log (Age);
 
+var Num1 = 10;
 
+var Num2 = 2;
 
-prompt("Welcome to my program " +userName+ "!\nHow old are you?", 15);
-prompt("Thats great " +userName+ "!\nHow much pocket money do you have?", "5 dollars");
-alert("So if I'm not mistaken")
-alert("As of " +Year+ " you are " +Age+ " years old\nYou were born in 2010\nIn 10 years you will be 25 years old\nYou have " +Pocketmoney+ " dollars\nHalf of your pocket money is 2.5 dollars");
+var answer;
+
+ 
+ 
+
+ /****************************
+  Main code 
+ ****************************/
+var Age = 15;
+var Year = 2025;
+let pocketMoney =3;
+var Born = 2010;
+
+  console.log("pocketMoney")
+  console.log("pocketMoney")
+  console.log("Hi Jason");
+  console.log("As of", Year, "You are", Age);
+  console.log("You were born in", Born)
+  answer = Age + Num1;
+  console.log("In 10 years you will be", answer, "year old")
+  console.log("You have", pocketMoney,"dollars");
+  answer = pocketMoney/Num2
+  console.log("Half of your money is",answer )
+
+  var username =( "Yuvi the loner")
+  var newName;
+  alert("Welcome to my program " + username + "!\n Please entire your name");
+  newName = prompt("What is your name?");
+  prompt(" Please entire your age ");
+  prompt(" how much pocket money do you have?");
+
+  var ageingten = Age + 10;
+  alert("Hi " + newName + "\nas of 2/5/2025 you are " + Age + " years old!\nYou were born in " + Born + "\nIn ten years you will be " + ageingten + " years old!");
